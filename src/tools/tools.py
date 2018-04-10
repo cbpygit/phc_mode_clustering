144b3142cac4dc3f629f374b91fd20c83884ac45
Carlo Barth <carlo.barth@helmholtz-berlin.de>
2018-04-10 08:36:28 +0200

    Moved silhouette implementation of Alexandre Abraham to its own file.

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -13,13 +13,11 @@ import numpy as np
 import pandas as pd
 
 from scipy.linalg import expm, norm
+from silhouette_implementations import silhouette_samples_block
 from sklearn import cluster, mixture, preprocessing
 from sklearn.metrics import silhouette_samples, silhouette_score
-from sklearn.metrics.pairwise import euclidean_distances, distance_metrics, \
-    pairwise_distances
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.externals import joblib
-from sklearn.utils import check_random_state
-from sklearn.externals.joblib import Parallel, delayed
 
 # Global constants
 DEFAULT_SIM_DDICT = {'Gamma-K': 0., 'Gamma-M': 90.}
@@ -721,203 +719,6 @@ def classify_with_model(sim_data_, model, pols=None, direcs=None,
     return sim_data, dict(sim_num_data)
 
 
-def silhouette_score_block(X, labels, metric='euclidean', sample_size=None,
-                           random_state=None, n_jobs=1, **kwds):
-    """Compute the mean Silhouette Coefficient of all samples.
-    The Silhouette Coefficient is calculated using the mean intra-cluster
-    distance (a) and the mean nearest-cluster distance (b) for each sample.
-    The Silhouette Coefficient for a sample is ``(b - a) / max(a, b)``.
-    To clarrify, b is the distance between a sample and the nearest cluster
-    that b is not a part of.
-    This function returns the mean Silhoeutte Coefficient over all samples.
-    To obtain the values for each sample, use silhouette_samples
-    The best value is 1 and the worst value is -1. Values near 0 indicate
-    overlapping clusters. Negative values generally indicate that a sample has
-    been assigned to the wrong cluster, as a different cluster is more similar.
-    Parameters
-    ----------
-    X : array [n_samples_a, n_features]
-        Feature array.
-    labels : array, shape = [n_samples]
-             label values for each sample
-    metric : string, or callable
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by metrics.pairwise.pairwise_distances. If X is the distance
-        array itself, use "precomputed" as the metric.
-    sample_size : int or None
-        The size of the sample to use when computing the Silhouette
-        Coefficient. If sample_size is None, no sampling is used.
-    random_state : integer or numpy.RandomState, optional
-        The generator used to initialize the centers. If an integer is
-        given, it fixes the seed. Defaults to the global numpy random
-        number generator.
-    `**kwds` : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a scipy.spatial.distance metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-    Returns
-    -------
-    silhouette : float
-        Mean Silhouette Coefficient for all samples.
-    References
-    ----------
-    Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
-        Interpretation and Validation of Cluster Analysis". Computational
-        and Applied Mathematics 20: 53-65. doi:10.1016/0377-0427(87)90125-7.
-    http://en.wikipedia.org/wiki/Silhouette_(clustering)
-    """
-    if sample_size is not None:
-        random_state = check_random_state(random_state)
-        indices = random_state.permutation(X.shape[0])[:sample_size]
-        if metric == "precomputed":
-            raise ValueError('Distance matrix cannot be precomputed')
-        else:
-            X, labels = X[indices], labels[indices]
-    return np.mean(silhouette_samples_block(
-        X, labels, metric=metric, n_jobs=n_jobs, **kwds))
-
-
-def silhouette_samples_block(X, labels, metric='euclidean', n_jobs=1, **kwds):
-    """Compute the Silhouette Coefficient for each sample.
-    The Silhoeutte Coefficient is a measure of how well samples are clustered
-    with samples that are similar to themselves. Clustering models with a high
-    Silhouette Coefficient are said to be dense, where samples in the same
-    cluster are similar to each other, and well separated, where samples in
-    different clusters are not very similar to each other.
-    The Silhouette Coefficient is calculated using the mean intra-cluster
-    distance (a) and the mean nearest-cluster distance (b) for each sample.
-    The Silhouette Coefficient for a sample is ``(b - a) / max(a, b)``.
-    This function returns the Silhoeutte Coefficient for each sample.
-    The best value is 1 and the worst value is -1. Values near 0 indicate
-    overlapping clusters.
-    Parameters
-    ----------
-    X : array [n_samples_a, n_features]
-        Feature array.
-    labels : array, shape = [n_samples]
-             label values for each sample
-    metric : string, or callable
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by metrics.pairwise.pairwise_distances. If X is the distance
-        array itself, use "precomputed" as the metric.
-    `**kwds` : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a scipy.spatial.distance metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-    Returns
-    -------
-    silhouette : array, shape = [n_samples]
-        Silhouette Coefficient for each samples.
-    References
-    ----------
-    Peter J. Rousseeuw (1987). "Silhouettes: a Graphical Aid to the
-        Interpretation and Validation of Cluster Analysis". Computational
-        and Applied Mathematics 20: 53-65. doi:10.1016/0377-0427(87)90125-7.
-    http://en.wikipedia.org/wiki/Silhouette_(clustering)
-    """
-    A = _intra_cluster_distances_block(X, labels, metric, n_jobs=n_jobs,
-                                       **kwds)
-    B = _nearest_cluster_distance_block(X, labels, metric, n_jobs=n_jobs,
-                                        **kwds)
-    sil_samples = (B - A) / np.maximum(A, B)
-    # nan values are for clusters of size 1, and should be 0
-    return np.nan_to_num(sil_samples)
-
-
-def _intra_cluster_distances_block_(subX, metric, **kwds):
-    distances = pairwise_distances(subX, metric=metric, **kwds)
-    return distances.sum(axis=1) / (distances.shape[0] - 1)
-
-
-def _intra_cluster_distances_block(X, labels, metric, n_jobs=1, **kwds):
-    """Calculate the mean intra-cluster distance for sample i.
-    Parameters
-    ----------
-    X : array [n_samples_a, n_features]
-        Feature array.
-    labels : array, shape = [n_samples]
-        label values for each sample
-    metric : string, or callable
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by metrics.pairwise.pairwise_distances. If X is the distance
-        array itself, use "precomputed" as the metric.
-    `**kwds` : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a scipy.spatial.distance metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-    Returns
-    -------
-    a : array [n_samples_a]
-        Mean intra-cluster distance
-    """
-    intra_dist = np.zeros(labels.size, dtype=float)
-    values = Parallel(n_jobs=n_jobs)(
-            delayed(_intra_cluster_distances_block_)
-                (X[np.where(labels == label)[0]], metric, **kwds)
-                for label in np.unique(labels))
-    for label, values_ in zip(np.unique(labels), values):
-        intra_dist[np.where(labels == label)[0]] = values_
-    return intra_dist
-
-
-def _nearest_cluster_distance_block_(subX_a, subX_b, metric, **kwds):
-    dist = pairwise_distances(subX_a, subX_b, metric=metric, **kwds)
-    dist_a = dist.mean(axis=1)
-    dist_b = dist.mean(axis=0)
-    return dist_a, dist_b
-
-
-def _nearest_cluster_distance_block(X, labels, metric, n_jobs=1, **kwds):
-    """Calculate the mean nearest-cluster distance for sample i.
-    Parameters
-    ----------
-    X : array [n_samples_a, n_features]
-        Feature array.
-    labels : array, shape = [n_samples]
-        label values for each sample
-    metric : string, or callable
-        The metric to use when calculating distance between instances in a
-        feature array. If metric is a string, it must be one of the options
-        allowed by metrics.pairwise.pairwise_distances. If X is the distance
-        array itself, use "precomputed" as the metric.
-    `**kwds` : optional keyword parameters
-        Any further parameters are passed directly to the distance function.
-        If using a scipy.spatial.distance metric, the parameters are still
-        metric dependent. See the scipy docs for usage examples.
-    X : array [n_samples_a, n_features]
-        Feature array.
-    Returns
-    -------
-    b : float
-        Mean nearest-cluster distance for sample i
-    """
-    inter_dist = np.empty(labels.size, dtype=float)
-    inter_dist.fill(np.inf)
-    # Compute cluster distance between pairs of clusters
-    unique_labels = np.unique(labels)
-
-    values = Parallel(n_jobs=n_jobs)(
-            delayed(_nearest_cluster_distance_block_)(
-                X[np.where(labels == label_a)[0]],
-                X[np.where(labels == label_b)[0]],
-                metric, **kwds)
-                for label_a, label_b in combinations(unique_labels, 2))
-
-    for (label_a, label_b), (values_a, values_b) in \
-            zip(combinations(unique_labels, 2), values):
-
-            indices_a = np.where(labels == label_a)[0]
-            inter_dist[indices_a] = np.minimum(values_a, inter_dist[indices_a])
-            del indices_a
-            indices_b = np.where(labels == label_b)[0]
-            inter_dist[indices_b] = np.minimum(values_b, inter_dist[indices_b])
-            del indices_b
-    return inter_dist
-
-
 def mini_test(every):
     print 'Loading simulation data, using every', every, 'angles and wavelength.'
     sim_data_init = get_results(every, every)
@@ -931,15 +732,14 @@ def mini_test(every):
     pol_suf = pdict[pol]
     theta_split = None
     field_type = 'electric'
-    cluster_type = 'BayesianGaussianMixture'
+    cluster_type = 'GaussianMixture'
 
     # Gaussian Mixture settings -> every = 5 works
     common_clkws = dict(covariance_type='tied',
                         max_iter=20,
-                        weight_concentration_prior=1.e-15,
                         treat_complex=None,
                         preprocess='normalize',
-                        random_state=0)  # <- for reproducability
+                        random_state=0)  # <- for reproducibility
 
     clkw_dicts = defaultdict(dict)
     for direc_ in ddict:
@@ -949,7 +749,7 @@ def mini_test(every):
     # Individual settings
     clkw_dicts['Gamma-K']['TE']['n_components'] = 8
     clkw_dicts['Gamma-K']['TM']['n_components'] = 8  # maybe 8, before: 7
-    clkw_dicts['Gamma-M']['TE']['n_components'] = 16  # maybe 8, before: 7
+    clkw_dicts['Gamma-M']['TE']['n_components'] = 7  # maybe 8, before: 7
     clkw_dicts['Gamma-M']['TM']['n_components'] = 7
     clkw_dicts = dict(clkw_dicts)
 
@@ -958,8 +758,8 @@ def mini_test(every):
                                              theta_split=theta_split,
                                              cluster_type=cluster_type,
                                              cluster_kwargs_dicts=clkw_dicts,
-                                             pols=[pol],
-                                             direcs=[direc],
+                                             # pols=[pol],
+                                             # direcs=[direc],
                                              field_type=field_type)
 
 
