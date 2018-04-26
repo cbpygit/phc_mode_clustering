f56715221ab27a2b42cd53e3a3cc7f605c61f5b5
Carlo Barth <carlo.barth@helmholtz-berlin.de>
2018-04-26 10:59:28 +0200

    Removed unused imports. Extended data loading test to all databases. Added a test suite for the complete clustering, using kmeans and GMM.

--- src/tests/test_base.py
+++ src/tests/test_base.py
@@ -10,7 +10,6 @@ import logging
 import os
 import sys
 
-from collections import defaultdict
 from copy import deepcopy
 from datetime import date
 
@@ -20,10 +19,8 @@ PROJECT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__),
                                             os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 from src.tools import tools
-from src.in_out import in_out
 
 MODEL_DIR = os.path.join(PROJECT_DIR, 'models')
-from shutil import rmtree
 import unittest
 
 
@@ -45,8 +42,92 @@ class TestDataLoading(unittest.TestCase):
 
     def test_load_field_data(self):
         sim_data = tools.get_results()
-        data = tools.load_field_data_for_sims_(sim_data.index, 0, 'electric')
-        self.assertItemsEqual(data.shape, (882, 603))
+        for ipol in [0, 1]:
+            for ftype in ['electric', 'magnetic']:
+                data = tools.load_field_data_for_sims(sim_data.index, ipol,
+                                                      ftype)
+                self.assertItemsEqual(data.shape, (882, 603))
+
+    def test_cache_usage(self):
+        sim_data = tools.get_results()
+        tools.set_cache(True)
+        tools.load_field_data_for_sims(sim_data.index, 0, 'electric')
+        tools.load_field_data_for_sims(sim_data.index, 0, 'electric')
+        tools.set_cache(False)
+
+
+# ==============================================================================
+class TestClustering(unittest.TestCase):
+
+    def setUp(self):
+        tools.set_dummy_mode(True, False)
+
+    def tearDown(self):
+        tools.set_dummy_mode(False, False)
+
+    def test_MiniBatchKMeans(self):
+        sim_data_init = tools.get_results()
+
+        # Define init parameters
+        ddict = tools.DEFAULT_SIM_DDICT
+        pdict = tools.DEFAULT_SIM_PDICT
+        field_type = 'electric'
+        cluster_type = 'MiniBatchKMeans'
+
+        # Type `tools.get_clustering_input_data?` for info on these parameters
+        common_clkws = dict(treat_complex=None,
+                            preprocess='normalize',
+                            random_state=0)  # <- for reproducibility
+
+        # Set these defaults for all combinations of direction and polarization
+        clkw_dicts = tools.defaultdict(dict)
+        for direc_ in ddict:
+            for pol_ in pdict:
+                clkw_dicts[direc_][pol_] = deepcopy(common_clkws)
+
+        # Set individual parameters, in this case for the number of clusters
+        clkw_dicts['Gamma-K']['TE']['n_clusters'] = 8
+        clkw_dicts['Gamma-K']['TM']['n_clusters'] = 8
+        clkw_dicts['Gamma-M']['TE']['n_clusters'] = 7
+        clkw_dicts['Gamma-M']['TM']['n_clusters'] = 7
+        clkw_dicts = dict(clkw_dicts)
+
+        tools.cluster_all_modes(sim_data_init, cluster_type=cluster_type,
+                                cluster_kwargs_dicts=clkw_dicts,
+                                field_type=field_type)
+
+    def test_GMM(self):
+        sim_data_init = tools.get_results()
+
+        # Define init parameters
+        ddict = tools.DEFAULT_SIM_DDICT
+        pdict = tools.DEFAULT_SIM_PDICT
+        field_type = 'electric'
+        cluster_type = 'GaussianMixture'
+
+        # Type `tools.get_clustering_input_data?` for info on these parameters
+        common_clkws = dict(covariance_type='tied',
+                            max_iter=20,
+                            treat_complex=None,
+                            preprocess='normalize',
+                            random_state=0)  # <- for reproducibility
+
+        # Set these defaults for all combinations of direction and polarization
+        clkw_dicts = tools.defaultdict(dict)
+        for direc_ in ddict:
+            for pol_ in pdict:
+                clkw_dicts[direc_][pol_] = deepcopy(common_clkws)
+
+        # Set individual parameters, in this case for the number of clusters
+        clkw_dicts['Gamma-K']['TE']['n_components'] = 8
+        clkw_dicts['Gamma-K']['TM']['n_components'] = 8  # maybe 8, before: 7
+        clkw_dicts['Gamma-M']['TE']['n_components'] = 7  # maybe 8, before: 7
+        clkw_dicts['Gamma-M']['TM']['n_components'] = 7
+        clkw_dicts = dict(clkw_dicts)
+
+        tools.cluster_all_modes(sim_data_init, cluster_type=cluster_type,
+                                cluster_kwargs_dicts=clkw_dicts,
+                                field_type=field_type)
 
 
 if __name__ == '__main__':
@@ -58,7 +139,8 @@ if __name__ == '__main__':
 
     # list of all test suites
     suites = [
-        unittest.TestLoader().loadTestsFromTestCase(TestDataLoading)]
+        unittest.TestLoader().loadTestsFromTestCase(TestDataLoading),
+        unittest.TestLoader().loadTestsFromTestCase(TestClustering)]
 
     # Get a log file for the test output
     log_dir = os.path.abspath('logs')
